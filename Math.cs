faa3203a9af1adf1f0cddcb865c0063252ba25fd
Razmik Anaxasyan <razmikanaxasyan000-555@mail.ru>
2021-10-09 23:36:25 +0400

    Math.cs Added

--- Math.cs
+++ Math.cs
@@ -14,16 +14,33 @@ namespace Homweork
         {
             string input = File.ReadAllText("Math.txt").ToString();
             
-                Regex pattern = new Regex(@"\d[]");
+                Regex pattern = new Regex(@"(\d{1,})(\+|\-|\/|\*)(\d{1,})");
           
             
             
             MatchCollection matches = pattern.Matches(input);
             foreach (Match match in matches)
             {
+            
+                switch (match.Groups[2].Value.ToString())
+                {
+                    case "+":
+                        Console.WriteLine($"{ match.Groups[0].Value} = {int.Parse(match.Groups[1].Value) + int.Parse(match.Groups[3].Value)}\n");
+                        break;
+                    case "-":
+                        Console.WriteLine($"{ match.Groups[0].Value} = {int.Parse(match.Groups[1].Value) - int.Parse(match.Groups[3].Value)}\n");
+                        break;
+                    case "*":
+                        Console.WriteLine($"{ match.Groups[0].Value} = {int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[3].Value)}\n");
+                        break;
+                    case "/":
+                        Console.WriteLine($"{ match.Groups[0].Value} = {int.Parse(match.Groups[1].Value) / int.Parse(match.Groups[3].Value)}\n");
+                        break;
 
-                Console.WriteLine(match.Value);
+                }
                 
+                
+
             }
         }
     }
